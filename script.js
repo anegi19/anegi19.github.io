9d94380f7c0fb10ddf963ffbb75ea02861dc116b
Akira-exo <anegi1919@gmail.com>
2026-07-07 19:54:43 +0200

    changed the bkg image with menu clicks

--- script.js
+++ script.js
@@ -1,29 +1,38 @@
 // Wait for the document to load before running the script 
 (function ($) {
-  
-  // We use some Javascript and the URL #fragment to hide/show different parts of the page
-  // https://developer.mozilla.org/en-US/docs/Web/HTML/Element/a#Linking_to_an_element_on_the_same_page
-  $(window).on('load hashchange', function(){
-    
-    // First hide all content regions, then show the content-region specified in the URL hash 
-    // (or if no hash URL is found, default to first menu item)
-    $('.content-region').hide();
-    
-    // Remove any active classes on the main-menu
-    $('.main-menu a').removeClass('active');
-    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
-    
-    // Now show the region specified in the URL hash
-    $(region).show();
-    
-    // Highlight the menu link associated with this region by adding the .active CSS class
-    $('.main-menu a[href="'+ region +'"]').addClass('active'); 
 
-    // Alternate method: Use AJAX to load the contents of an external file into a div based on URL fragment
-    // This will extract the region name from URL hash, and then load [region].html into the main #content div
-    // var region = location.hash.toString() || '#first';
-    // $('#content').load(region.slice(1) + '.html')
-    
+  $(window).on('load hashchange', function(){
+
+    // Hide all content regions
+    $('.content-region').hide();
+
+    // Remove active menu styles
+    $('.main-menu a').removeClass('active');
+
+    // Get selected section
+    var region = location.hash.toString() || $('.main-menu a:first').attr('href');
+
+    // Show selected section
+    $(region).show();
+
+    // Highlight selected menu
+    $('.main-menu a[href="'+ region +'"]').addClass('active');
+
+
+    // -------- Ocean background change --------
+
+    if (region === "#home") {
+      document.documentElement.className = "bg-home";
+    }
+
+    else if (region === "#about") {
+      document.documentElement.className = "bg-about";
+    }
+
+    else if (region === "#contact") {
+      document.documentElement.className = "bg-contact";
+    }
+
   });
-  
+
 })(jQuery);
